59acd875d1af4c33a0aba7569f590133077d8a01
Nada Yehia Dawoud <nadawoud@gmail.com>
2021-03-23 23:33:20 +0200

    Create Card model

---
+++ Cardi/Model/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Cardi
+//
+//  Created by Nada Yehia Dawoud on 23/03/2021.
+//
+
+import Foundation
+
+struct Card {
+    var title: String
+    var description: String?
+    var backTitle: String?
+    var backDescription: String?
+    var emoji: String?
+}
