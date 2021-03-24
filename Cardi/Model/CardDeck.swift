fd27ba9562bd16eeca9cce52b293c14c7dd718c4
Nada Yehia Dawoud <nadawoud@gmail.com>
2021-03-24 19:48:11 +0200

    Create CardDeck model

---
+++ Cardi/Model/CardDeck.swift
@@ -0,0 +1,14 @@
+//
+//  CardDeck.swift
+//  Cardi
+//
+//  Created by Nada Yehia Dawoud on 24/03/2021.
+//
+
+import Foundation
+
+struct CardDeck {
+    var title: String
+    var cards: [Card]
+    var coverEmoji: String?
+}
