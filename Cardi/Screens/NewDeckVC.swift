0076149f2c5e753d2f7382506df7ba6166d1281b
Nada Yehia Dawoud <nadawoud@gmail.com>
2021-06-24 14:01:13 +0200

    Create NewDeckVC Swift file and Storyboard

---
+++ Cardi/Screens/NewDeckVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewDeckVC.swift
+//  Cardi
+//
+//  Created by Nada Yehia Dawoud on 16/06/2021.
+//
+
+import UIKit
+import Reusable
+
+class NewDeckVC: UIViewController, StoryboardBased {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
