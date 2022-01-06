12555bd5469f5881b348d34d9aeda93176da947b
Nada Yehia Dawoud <nadawoud@gmail.com>
2022-01-06 23:30:04 +0200

    Update EmojiPickerVC

--- Cardi/Screens/EmojiPickerVC.swift
+++ Cardi/Screens/EmojiPickerVC.swift
@@ -14,49 +14,59 @@ protocol EmojiPickerDelegate: AnyObject {
 
 
 class EmojiPickerVC: UIViewController, StoryboardBased {
-    let emoji = Emoji.shared
     
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var toolbar: UIToolbar!
     
+    var emojiCategories = [EmojiCategory]()
     weak var delegate: EmojiPickerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureToolbar()
+        guard let emojiCategories = getEmojisFromJSONFile(withName: "emoji") else { return }
+        self.emojiCategories = emojiCategories
     }
     
-    private func configureToolbar() {
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
-        toolbar.setItems([doneButton], animated: true)
-    }
-    
-    @objc func doneButtonTapped() {
-        dismiss(animated: true)
+    private func getEmojisFromJSONFile(withName fileName: String) -> [EmojiCategory]? {
+        let decoder = JSONDecoder()
+        guard
+            let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let emojiCategories = try? decoder.decode([EmojiCategory].self, from: data)
+        else {
+            return nil
+        }
+        return emojiCategories
     }
 }
 
 
 extension EmojiPickerVC: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        emoji.sections.count
+        emojiCategories.count
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let category = emoji.sections[section]
-        let emoji = self.emoji.data[category]
+        let category = emojiCategories[section]
+        let emojis = category.emojis
         
-        return emoji?.count ?? 0
+        return emojis.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let emojiCell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath) as! EmojiCell
-        let category = emoji.sections[indexPath.section]
-        let emoji = self.emoji.data[category]?[indexPath.item] ?? ""
+        let category = emojiCategories[indexPath.section]
+        let emoji = category.emojis[indexPath.item]
         
-        emojiCell.emojiLabel.text = emoji
+        emojiCell.emojiLabel.text = emoji.emoji
         
         return emojiCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let emojiHeaderView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: EmojiSectionHeaderView.reuseID, for: indexPath) as! EmojiSectionHeaderView
+        let category = emojiCategories[indexPath.section]
+        emojiHeaderView.headerLabel.text = category.title
+        return emojiHeaderView
     }
 }
 
@@ -75,15 +85,15 @@ extension EmojiPickerVC: UICollectionViewDelegate {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-      if section == 0 {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 8 / 2, right: 0)
-      } else {
-        return UIEdgeInsets(top: 8 / 2, left: 0, bottom: 8 / 2, right: 0)
-      }
+        if section == 0 {
+            return UIEdgeInsets(top: 0, left: 0, bottom: 8 / 2, right: 0)
+        } else {
+            return UIEdgeInsets(top: 8 / 2, left: 0, bottom: 8 / 2, right: 0)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let emoji = emoji.emoji(at: indexPath) else { return }
+        let emoji = emojiCategories[indexPath.section].emojis[indexPath.item].emoji
         delegate?.didPickEmoji(emoji)
         dismiss(animated: true)
     }
