b0da0b6d70e8185e10ab6c2cca1233722f7a4211
Nada Yehia Dawoud <nadawoud@gmail.com>
2022-01-06 23:27:41 +0200

    Create custom CollectionView header

---
+++ Cardi/Custom Views/EmojiSectionHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiSectionHeaderView.swift
+//  Cardi
+//
+//  Created by Nada Yehia Dawoud on 30/12/2021.
+//
+
+import UIKit
+
+class EmojiSectionHeaderView: UICollectionReusableView {
+    static let reuseID = "EmojiSectionHeaderView"
+    
+    @IBOutlet weak var headerLabel: UILabel!
+}
