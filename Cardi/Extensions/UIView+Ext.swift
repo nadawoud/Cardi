879adf2e396e1ea533ed2ca731ef33c42fbe4a5f
Nada Yehia Dawoud <nadawoud@gmail.com>
2022-02-26 22:25:54 +0200

    Extend UIView

---
+++ Cardi/Extensions/UIView+Ext.swift
@@ -0,0 +1,74 @@
+//
+//  UIView+Ext.swift
+//  Cardi
+//
+//  Created by Nada Yehia Dawoud on 26/02/2022.
+//
+
+import UIKit
+
+extension UIView {
+    // MARK: - Properties
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.borderColor ?? CGColor(gray: 1, alpha: 0))
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.shadowColor ?? CGColor(gray: 1, alpha: 0))
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+}
